98d598e6e0c5afd91cfab930db1ad14fa2b6a68a
paradoxabby <rajputabhishek4000@gmail.com>
2021-04-23 20:59:08 +0530

    new directories test

--- app/src/main/java/com/example/galaxy/CircleFragment.java
+++ app/src/main/java/com/example/galaxy/CircleFragment.java
@@ -1,4 +1,4 @@
-package com.example.galaxy.testpackge;
+package com.example.galaxy;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -7,8 +7,6 @@ import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
-import com.example.galaxy.R;
 
 
 public class CircleFragment extends Fragment {
